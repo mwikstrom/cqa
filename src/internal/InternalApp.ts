9ddc4a83ebe70125d8e8100207ac986393d9775b
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2018-05-08 22:45:29 +0200

    move internal code from app to query

--- src/internal/InternalApp.ts
+++ src/internal/InternalApp.ts
@@ -1,5 +1,4 @@
 import { App } from "../public/App";
-import { CancelToken } from "../public/CancelToken";
 import { CancelTokenSource } from "../public/CancelTokenSource";
 
 import { invariant } from "./Demand";
@@ -8,6 +7,10 @@ import { InternalQuery } from "./InternalQuery";
 
 export class InternalApp extends InternalOf<App> {
     private _observedQueries = new Map<string, Set<InternalQuery>>();
+
+    public getObservedQueries(key: string): Iterable<InternalQuery> {
+        return this._observedQueries.get(key) || [];
+    }
 
     public registerObservedQuery(
         query: InternalQuery,
@@ -25,114 +28,44 @@ export class InternalApp extends InternalOf<App> {
             );
         }
 
+        // Create a cancel token to be observed while populating. It will be cancelled when the query is unobserved.
         const cts = new CancelTokenSource();
 
-        // Add registered instance and start executing query on backend if it is the first instance
+        // Add registered instance and start populating query in a background task
         instances.add(query);
-        if (instances.size === 1) {
-            // TODO: Execute query on backend. Cancel token source when first snapshot arrives.
-        }
+        query.populateInBackground(cts.token);
 
-        // TODO: Must delay backend execution until query was populated locally so that we have a chance to get
-        //       catch-up deltas instead of a full initial snapshot.
-
-        // TODO: Handle non-cancellation exceptions too. Mark query as broken.
-        cts.token.ignoreCancellation(this._populateQuery(query, cts.token));
-
-        // Return the `registerUnobservedQuery` callback function
+        // Return a callback function that shall be invoked when the query becomes unobserved
         return () => {
-            // Cancel the populating task, if it is still pending
+            // Cancel the populating background task when query become unobserved
             cts.cancel();
 
             // Remove registered instance and stop executing query on backend if it was the last instance
-            instances!.delete(query);
-            if (instances!.size === 0) {
-                // TODO: Stop query on backend.
+            if (instances!.delete(query) && instances!.size === 0) {
+                this.stopQuerySubscription(query.key);
             }
         };
     }
 
-    // TODO: Move to InternalQuery
-    private async _populateQuery(
-        query: InternalQuery,
-        token: CancelToken,
+    public startQuerySubscription(
+        key: string,
     ) {
-        if (this._tryPopulateQueryFromClone(query)) {
-            return;
-        }
-
-        if (await this._tryPopulateQueryFromStore(query, token)) {
-            // TODO: Apply pending and unseen committed commands.
-            return;
-        }
-
-        if (!query.version) {
-            // TODO: This should not be attempted when there are unseen commits (from local commands)
-            //       or the derive function shall not be allowed to read from other queries that may
-            //       have unseen commits (from local commands).
-            await this._deriveQueryResultFromOther(query, token);
-        }
+        // TODO: IMPLEMENT startQuerySubscription
+        //       - Must verify that there is at least one instance for the specified key
+        //       - All instances must agree to:
+        //          a) use the same key
+        //          b) use the same descriptor
+        //          c) support incremental updates
+        //          d) have the same current version
+        //          e) not be broken
+        throw key;
     }
 
-    // TODO: Move to InternalQuery
-    private _tryPopulateQueryFromClone(
-        query: InternalQuery,
+    public stopQuerySubscription(
+        key: string,
     ) {
-        // Look for another observed query instance with the same key
-        const source = this._findFirstQueryClone(query);
-        if (!source) {
-            return false;
-        }
-
-        // Instances that does not have a version token are either hollow or are derived from other queries locally,
-        // we don't want to (and cannot) populate from such a clone.
-        const version = source.version;
-        if (!version) {
-            return false;
-        }
-
-        // TODO: This should not be attempted when clone has unseen commits (from local commands)
-        //       -or- we must support cloning of those commands
-
-        const snapshot = source.pub.tryBuildSnapshot();
-        if (snapshot === undefined) {
-            return false;
-        }
-
-        query.applySnapshot(snapshot, version);
-        return true;
-    }
-
-    // TODO: Move to InternalQuery
-    private _findFirstQueryClone(
-        query: InternalQuery,
-    ): InternalQuery | null {
-        const instances = this._observedQueries.get(query.key)!;
-
-        for (const other of instances) {
-            if (other !== query) {
-                return other;
-            }
-        }
-
-        return null;
-    }
-
-    // TODO: Move to InternalQuery
-    private async _tryPopulateQueryFromStore(
-        query: InternalQuery,
-        token: CancelToken,
-    ): Promise<boolean> {
-        // TODO: IMPLEMENT
-        return !!query && !!token; // dummy
-    }
-
-    // TODO: Move to InternalQuery
-    private async _deriveQueryResultFromOther(
-        query: InternalQuery,
-        token: CancelToken,
-    ): Promise<boolean> {
-        // TODO: IMPLEMENT
-        return !!query && !!token; // dummy
+        // TODO: Verify that there are instances for the specified key!
+        // TODO: IMPLEMENT stopQuerySubscription
+        throw key;
     }
 }
--- src/internal/InternalQuery.ts
+++ src/internal/InternalQuery.ts
@@ -3,14 +3,19 @@ import {
     createAtom,
     IAtom,
     observable,
+    when,
 } from "mobx";
 
+import { App } from "../public/App";
+import { CancelToken } from "../public/CancelToken";
+import { CancelTokenSource } from "../public/CancelTokenSource";
 import { ReadonlyJsonValue } from "../public/Json";
 import { Query } from "../public/Query";
 import { Version } from "../public/Version";
 
 import { demand } from "./Demand";
-import { InternalOf } from "./InternalOf";
+import { InternalApp } from "./InternalApp";
+import { InternalOf, makeInternalOf } from "./InternalOf";
 
 export class InternalQuery extends InternalOf<Query> {
     private _atom: IAtom;
@@ -93,8 +98,60 @@ export class InternalQuery extends InternalOf<Query> {
         this._version = version;
     }
 
+    public populateInBackground(
+        token: CancelToken,
+    ): void {
+        token.ignoreCancellation(this._populate(token)).catch(reason => {
+            // TODO: Report error and mark query as broken
+            // tslint:disable-next-line
+            console.error(reason); // report better!
+        });
+    }
+
     public reportObserved() {
         this._atom.reportObserved();
+    }
+
+    private async _deriveFromOther(
+        token: CancelToken,
+    ): Promise<void> {
+        const cts = new CancelTokenSource();
+        const disposeReaction = when(
+            () => token.isCancelled || this.version !== null,
+            () => cts.cancel(),
+        );
+
+        try {
+            const applySnapshot = (data: ReadonlyJsonValue) => {
+                cts.token.throwIfCancelled();
+                this.pub.onSnapshot(data);
+            };
+
+            const applyUpdate = (data: ReadonlyJsonValue) => {
+                cts.token.throwIfCancelled();
+                this.pub.onUpdate(data);
+            };
+
+            return await cts.token.ignoreCancellation(
+                this.pub.deriveLocalResult(
+                    applySnapshot,
+                    applyUpdate,
+                    cts.token,
+                ),
+            );
+        } finally {
+            disposeReaction();
+        }
+    }
+
+    private _findFirstClone(): InternalQuery | null {
+        for (const other of internalAppOf(this).getObservedQueries(this.key)) {
+            if (other !== this) {
+                return other;
+            }
+        }
+
+        return null;
     }
 
     private _onBecomeObserved = () => {
@@ -104,4 +161,62 @@ export class InternalQuery extends InternalOf<Query> {
     private _onBecomeUnobserved = () => {
         this._isObserved = false;
     }
+
+    private async _populate(
+        token: CancelToken,
+    ): Promise<void> {
+        // The fastest way to populate a query is to copy from a clone, another observed query instance with the same
+        // key as this query instance. Attempting to do so is a synchronous operation.
+        if (this._tryPopulateFromClone()) {
+            return; // Done. Populated from a clone.
+        }
+
+        // TODO: Look for and register applicable pending and unseen committed commands.
+
+        await this._tryPopulateFromStore(token);
+
+        internalAppOf(this).startQuerySubscription(this.key);
+
+        if (!this.version) {
+            await this._deriveFromOther(token);
+        }
+    }
+
+    private _tryPopulateFromClone(): boolean {
+        // Look for another observed query instance with the same key
+        const source = this._findFirstClone();
+        if (!source) {
+            return false;
+        }
+
+        // Instances that does not have a version token are either hollow or are derived from other queries locally,
+        // we don't want to (and cannot) populate from such a clone.
+        const version = source.version;
+        if (!version) {
+            return false;
+        }
+
+        const snapshot = source.pub.tryBuildSnapshot();
+        if (snapshot === undefined) {
+            return false;
+        }
+
+        // TODO: Clone pending and unseen committed commands too! (but don't reapply them!)
+
+        this.applySnapshot(snapshot, version);
+        return true;
+    }
+
+    private async _tryPopulateFromStore(
+        token: CancelToken,
+    ): Promise<boolean> {
+        // TODO: IMPLEMENT
+        //       - Beware that server snapshot or update might arrive while loading from store!
+        //       - Remember to re-apply pending and unseen committed commands too!
+        return !token; // dummy
+    }
 }
+
+const appInternalOf = makeInternalOf(App, InternalApp);
+
+const internalAppOf = (query: InternalQuery) => appInternalOf(query.pub.app);
