57dd35176fbaf29ac59e0363f19631b73b670953
Mårten Wikström <>
2018-05-17 23:09:00 +0200

    test

--- src/api/UnknownQuery.test.ts
+++ src/api/UnknownQuery.test.ts
@@ -2,7 +2,7 @@ import { UnknownQuery } from "../api";
 
 describe("UnknownQuery", () => {
     it("does not support incremental updates", () => {
-        const instance = new UnknownQuery(null, "");
+        const instance = new UnknownQuery(null);
         expect(instance.supportsIncrementalUpdates).toBe(false);
     });
 
@@ -14,5 +14,16 @@ describe("UnknownQuery", () => {
     it("returns the key given to constructor", () => {
         const instance = new UnknownQuery(null, "test");
         expect(instance.buildKey()).toBe("test");
+    });
+
+    it("cannot provide a snapshot when hollow", () => {
+        const instance = new UnknownQuery(null);
+        expect(instance.tryBuildSnapshot()).toBeUndefined();
+    });
+
+    it("can provide a snapshot after being populated", () => {
+        const instance = new UnknownQuery(null);
+        instance.onSnapshot({ hello: "world" });
+        expect(instance.tryBuildSnapshot()).toMatchObject({ hello: "world" });
     });
 });
