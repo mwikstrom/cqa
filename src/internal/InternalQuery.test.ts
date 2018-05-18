c884ab79b0532f0ee50cc8bb26a47763d15f5478
Mårten Wikström <>
2018-05-18 14:56:15 +0200

    test populate from clone

--- src/internal/InternalQuery.test.ts
+++ src/internal/InternalQuery.test.ts
@@ -1,4 +1,10 @@
 import {
+    autorun,
+    when,
+} from "mobx";
+
+import {
+    App,
     UnknownQuery,
 } from "../api";
 
@@ -29,5 +35,24 @@ describe("InternalQuery", () => {
         expect(instance.version).toBe("v1");
         instance.applyVersion("v2");
         expect(instance.version).toBe("v2");
+    });
+
+    it("can be populated from a clome", async () => {
+        const q1 = internalOf(new UnknownQuery(null));
+        const q2 = internalOf(new UnknownQuery(null));
+        const app = new App();
+        const stop = autorun(() => {
+            q1.reportObserved();
+            q2.reportObserved();
+        });
+
+        q1.applySnapshot("hello world", "v1");
+        q1.pub.attachTo(app);
+
+        q2.pub.attachTo(app);
+        await when(() => !q2.isPopulating);
+
+        expect(q2.pub.tryBuildSnapshot()).toBe("hello world");
+        expect(q2.version).toBe("v1");
     });
 });
