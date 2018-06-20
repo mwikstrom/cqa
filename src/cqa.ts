e2a8fe0131066c7b6ca1d0579799d9fadbf47093
Mårten Wikström <marten@marlin.nu>
2018-06-20 13:10:37 +0200

    reorder

--- src/cqa.ts
+++ src/cqa.ts
@@ -4,6 +4,34 @@
  * https://github.com/mwikstrom/cqa
  */
 
+//
+// Make sure that we have a `process.env` object. We'll read `NODE_ENV` from that object to distinguish between
+// production and non-production (DEBUG) bundles.
+//
+if (typeof process !== "object") {
+    // @ts-ignore
+    process = { env: { } };
+} else if (typeof process.env !== "object") {
+    process.env = {};
+}
+
+//
+// Print a warning when a DEBUG mode (non-production) bundle is minified.
+//
+(() => {
+    function this_name_shall_be_minified_in_production() { /* no-op */ }
+
+    if (DEBUG && this_name_shall_be_minified_in_production.name !== "this_name_shall_be_minified_in_production") {
+        /* tslint:disable-next-line */
+        console.warn(`[${LIB_NAME_SHORT}]: you should set 'process.env.NODE_ENV' to 'production' in your bundler.`);
+    }
+})();
+
+import {
+    DEBUG,
+    LIB_NAME_SHORT,
+} from "./internal/const";
+
 export interface ICommandType<TParam = any> {
     readonly name: string;
     createDefaultParam(): TParam;
@@ -154,31 +182,3 @@ export function getQueryStatus<TParam = any, TResult = any>(
 // TODO: function getLocalQueryResultAsync
 
 // TODO: function getFreshQueryResultAsync
-
-//
-// Make sure that we have a `process.env` object. We'll read `NODE_ENV` from that object to distinguish between
-// production and non-production (DEBUG) bundles.
-//
-if (typeof process !== "object") {
-    // @ts-ignore
-    process = { env: { } };
-} else if (typeof process.env !== "object") {
-    process.env = {};
-}
-
-//
-// Print a warning when a DEBUG mode bundle (non-production) is minified.
-//
-(() => {
-    function this_name_shall_be_minified_in_production() { /* no-op */ }
-
-    if (DEBUG && this_name_shall_be_minified_in_production.name !== "this_name_shall_be_minified_in_production") {
-        /* tslint:disable-next-line */
-        console.warn(`[${LIB_NAME_SHORT}]: you should set 'process.env.NODE_ENV' to 'production' in your bundler.`);
-    }
-})();
-
-import {
-    DEBUG,
-    LIB_NAME_SHORT,
-} from "./internal/const";
