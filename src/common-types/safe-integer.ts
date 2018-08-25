eec8b8c028d1a9401f8d58358a06202920d6b937
Mårten Wikström <marten@marlin.nu>
2018-08-25 21:29:34 +0200

    clean: remove unused safe integer

--- src/common-types/safe-integer.ts
+++
@@ -1,8 +0,0 @@
-import * as t from "io-ts";
-
-/** @internal */
-export const SafeInteger = t.refinement(
-    t.Integer,
-    value => value >= Number.MIN_SAFE_INTEGER && value <= Number.MAX_SAFE_INTEGER,
-    "safe integer",
-);
